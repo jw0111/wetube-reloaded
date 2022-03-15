e446528de99d6219178c903d61504981f4e3a76f
jw0111 <59572441+jw0111@users.noreply.github.com>
2022-03-15 17:17:56 +0900

    Chap 5

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,34 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send(`Watch : ${req.params.id}`);
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 4,
+      comments: 4,
+      createdAt: "5 minutes ago",
+      views: 21,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 3,
+      comments: 1,
+      createdAt: "3 minutes ago",
+      views: 33,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) =>
